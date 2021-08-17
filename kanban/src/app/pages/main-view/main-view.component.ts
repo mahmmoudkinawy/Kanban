dce5a7f1abf95d88bbce3ce5ab89b6016df362b8
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-08-17 21:08:49 +0200

    Added models & make it daynamic

--- kanban/src/app/pages/main-view/main-view.component.ts
+++ kanban/src/app/pages/main-view/main-view.component.ts
@@ -4,6 +4,8 @@ import {
   moveItemInArray,
   transferArrayItem,
 } from '@angular/cdk/drag-drop';
+import { Board } from 'src/app/models/board.model';
+import { Column } from 'src/app/models/column.model';
 
 @Component({
   selector: 'app-main-view',
@@ -13,11 +15,30 @@ import {
 export class MainViewComponent implements OnInit {
   constructor() {}
 
+  board: Board = new Board('Test Board', [
+    new Column('Ideas', [
+      'Some random idea',
+      'This is another random idea',
+      'Build an idea application',
+    ]),
+    new Column('Research', [
+      'Some random Research',
+      'This is another random Research',
+      'Build an Research application',
+    ]),
+    new Column('Todo', [
+      'Some random Todo',
+      'This is another random Todo',
+      'Build an Todo application',
+    ]),
+    new Column('Done', [
+      'Some random Done',
+      'This is another random Done',
+      'Build an Done application',
+    ]),
+  ]);
+
   ngOnInit(): void {}
-
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
